82a1d0679138410565e9ada1a7698dd294f55094
Matthew Green <matthew@newedgeengineering.com>
2013-11-05 17:32:52 +0000

    Added printout to be able to find database

--- src/adaptivelab/adaptivelab/settings.py
+++ src/adaptivelab/adaptivelab/settings.py
@@ -23,6 +23,7 @@ DATABASES = {
     }
 }
 
+print "Database can be found at %s" % DATABASES['default']['NAME']
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
