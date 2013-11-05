63ca720bbe5b543be19b73179492c72f017e36e1
Matthew Green <matthew@newedgeengineering.com>
2013-11-05 09:26:50 +0000

    Initial django project set up

--- README.md
+++ README.md
@@ -2,3 +2,42 @@
 
 # Introduction #
 Web application that displays tweets about Coke.
+
+The following instruction have only been executed on Mac OS X.
+
+## Execution ##
+
+### Requisites ###
+The following software is needed to execute this application:
+* Python 2.6
+* [virtualenv](http://pypi.python.org/pypi/virtualenv>)
+* [virtualenvwrapper](http://www.doughellmann.com/projects/virtualenvwrapper/)
+
+### Python Virtual Environment ###
+We deploy this application into it's own virtual environment therefore you will 
+need to do the same. Install & Activate the virtualenv (this assumes you are in 
+the root of the application folder using a cli):
+
+        easy_install virtualenv
+        mkdir ~/.virtualenvs
+        cd ~/.virtualenvs
+        virtualenv adaptivelab
+        source ~/.virtualenvs/adaptivelab/bin/activate
+
+### Install Python Packages ###
+This assumes you are in the root of the application folder using cli:
+
+        cd src
+        while read line; do easy_install -ZU $line; done < install-requires.txt
+        while read line; do easy_install -ZU $line; done < install-test-requires.txt
+        
+### Running Application ###
+Execute the following to get the application up and running (assumes you are in 
+the root of the project):
+
+        cd src/adaptivelab
+        python setup.py develop
+        python manage.py syncdb
+        python manage.py migrate
+        python manage.py runserver
+        
---
+++ src/adaptivelab/setup.py
@@ -0,0 +1,49 @@
+#  -*- coding: utf-8 -*- 
+"""
+Setup tools script for adaptive lab project.
+"""
+
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+def required(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read().split('\n')
+
+config = {
+    "name" : "adaptivelab",
+    "version" : "0.0.1",
+    "namespace_packages" : ['adaptivelab'],
+    "packages" : find_packages(exclude=[
+                                         "*.tests", "*.tests.*", "tests.*", "tests",
+                                         "*.ez_setup", "*.ez_setup.*", "ez_setup.*", "ez_setup",
+                                         "*.examples", "*.examples.*", "examples.*", "examples",
+                                       ]),
+    "include_package_data" : True,
+    "package_data" : { },
+    "scripts" : [ ],
+    "entry_points" : { },
+    "install_requires" : [  required('../install-requires.txt') ],
+    "tests_require" : [ required('../install-test-requires.txt') ],
+    "test_suite" : 'nose.collector',
+    "zip_safe" : False,
+    
+    # Metadata for upload to PyPI
+    "author" : 'Matthew Green',
+    "author_email" : "matthew@newedgeengineering.com",
+    "description" : "Technical Test",
+    "long_description" : "",
+    "classifiers" : [
+                      "Programming Language :: Python",
+                    ],
+    "license" : "",
+    "keywords" : "",
+    "url" : "",
+}
+
+setup(**config)
